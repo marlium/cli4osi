b12d8843192f977ff336dcf0065407193986b718
m4rl <85258950+marlium@users.noreply.github.com>
2022-02-09 03:31:05 +0000

    Update osi.nim

--- osi.nim
+++ osi.nim
@@ -1,1 +1,21 @@
+# Operating System Installer
 
+let
+    arch = ""
+    
+    debian = ""
+    
+    fedora = ""
+    
+    kali = ""
+    
+    manjaro = ""
+    
+    mint = ""
+    
+    popos = ""
+    
+    ubuntu = ""
+
+
+echo "\n  ~ CLI 4 OS Installation ~ "
